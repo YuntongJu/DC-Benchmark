0d2c4691a65ea2b5a20f5eb875ec48a4b4395e62
justincui03@gmail.com <justincui@tassadar.cs.ucla.edu>
2022-03-29 20:21:20 -0700

    Add file placeholder for different distillation methods.

--- evaluator/cross_arch_evaluator.py
+++ evaluator/cross_arch_evaluator.py
@@ -8,4 +8,9 @@ class CrossArchEvaluator(Evaluator):
         this.config = config
     
     def evaluate():
+        per_arch_accuracy = {}
+        for model_name in config.models:
+            model = EvaluatorUtils.create_network(model_name)
+            per_arch_accuracy[model_name] = EvaluatorUtils.evaluate_synset(model, input_images, input_labels, test_dataset)
+        return per_arch_accuracy
         
---
+++ methods/dataset_distillation.py
@@ -0,0 +1,1 @@
+dataset distillation.
