8645d5a085677a1f143b2fef62f0c28d641719f2
justincui03@gmail.com <justincui03@gmail.com>
2022-04-14 23:55:19 -0700

    working on trajectory matching.

--- distilled_results/TrajectoryMatching/tm_evaluator.py
+++ distilled_results/TrajectoryMatching/tm_evaluator.py
@@ -16,24 +16,67 @@ class CrossArchEvaluator(Evaluator):
 
     def prepare_args(self):
         parser = argparse.ArgumentParser(description='Parameter Processing')
-        parser.add_argument('--method', type=str, default='DM', help='DC/DSA/DM')
         parser.add_argument('--dataset', type=str, default='CIFAR10', help='dataset')
+
+        parser.add_argument('--subset', type=str, default='imagenette', help='ImageNet subset. This only does anything when --dataset=ImageNet')
+
         parser.add_argument('--model', type=str, default='ConvNet', help='model')
+
         parser.add_argument('--ipc', type=int, default=1, help='image(s) per class')
-        parser.add_argument('--eval_mode', type=str, default='S', help='eval_mode') # S: the same to training model, M: multi architectures,  W: net width, D: net depth, A: activation function, P: pooling layer, N: normalization layer,
-        parser.add_argument('--num_exp', type=int, default=5, help='the number of experiments')
-        parser.add_argument('--num_eval', type=int, default=20, help='the number of evaluating randomly initialized models')
-        parser.add_argument('--epoch_eval_train', type=int, default=300, help='epochs to train a model with synthetic data')
-        parser.add_argument('--Iteration', type=int, default=20000, help='training iterations')
-        parser.add_argument('--lr_img', type=float, default=1.0, help='learning rate for updating synthetic images')
-        parser.add_argument('--lr_net', type=float, default=0.01, help='learning rate for updating network parameters')
+
+        parser.add_argument('--eval_mode', type=str, default='S',
+                            help='eval_mode, check utils.py for more info')
+
+        parser.add_argument('--num_eval', type=int, default=5, help='how many networks to evaluate on')
+
+        parser.add_argument('--eval_it', type=int, default=100, help='how often to evaluate')
+
+        parser.add_argument('--epoch_eval_train', type=int, default=1000, help='epochs to train a model with synthetic data')
+        parser.add_argument('--Iteration', type=int, default=5000, help='how many distillation steps to perform')
+
+        parser.add_argument('--lr_img', type=float, default=1000, help='learning rate for updating synthetic images')
+        parser.add_argument('--lr_lr', type=float, default=1e-05, help='learning rate for updating... learning rate')
+        parser.add_argument('--lr_teacher', type=float, default=0.01, help='initialization for synthetic learning rate')
+        parser.add_argument('--lr_net', type=float, default=0.01, help='initialization for synthetic learning rate')
+
+        parser.add_argument('--lr_init', type=float, default=0.01, help='how to init lr (alpha)')
+
         parser.add_argument('--batch_real', type=int, default=256, help='batch size for real data')
+        parser.add_argument('--batch_syn', type=int, default=None, help='should only use this if you run out of VRAM')
         parser.add_argument('--batch_train', type=int, default=256, help='batch size for training networks')
-        parser.add_argument('--init', type=str, default='real', help='noise/real: initialize synthetic images from random noise or randomly sampled real images.')
-        parser.add_argument('--dsa_strategy', type=str, default='color_crop_cutout_flip_scale_rotate', help='differentiable Siamese augmentation strategy')
+
+        parser.add_argument('--pix_init', type=str, default='real', choices=["noise", "real"],
+                            help='noise/real: initialize synthetic images from random noise or randomly sampled real images.')
+
+        parser.add_argument('--dsa', type=str, default='True', choices=['True', 'False'],
+                            help='whether to use differentiable Siamese augmentation.')
+
+        parser.add_argument('--dsa_strategy', type=str, default='color_crop_cutout_flip_scale_rotate',
+                            help='differentiable Siamese augmentation strategy')
+
         parser.add_argument('--data_path', type=str, default='data', help='dataset path')
-        parser.add_argument('--save_path', type=str, default='result', help='path to save results')
-        parser.add_argument('--dis_metric', type=str, default='ours', help='distance metric')
+        parser.add_argument('--buffer_path', type=str, default='./buffers', help='buffer path')
+
+        parser.add_argument('--expert_epochs', type=int, default=3, help='how many expert epochs the target params are')
+        parser.add_argument('--syn_steps', type=int, default=20, help='how many steps to take on synthetic data')
+        parser.add_argument('--max_start_epoch', type=int, default=25, help='max epoch we can start at')
+
+        parser.add_argument('--zca', action='store_true', help="do ZCA whitening")
+
+        parser.add_argument('--load_all', action='store_true', help="only use if you can fit all expert trajectories into RAM")
+
+        parser.add_argument('--no_aug', type=bool, default=False, help='this turns off diff aug during distillation')
+
+        parser.add_argument('--texture', action='store_true', help="will distill textures instead")
+        parser.add_argument('--canvas_size', type=int, default=2, help='size of synthetic canvas')
+        parser.add_argument('--canvas_samples', type=int, default=1, help='number of canvas samples per iteration')
+
+
+        parser.add_argument('--max_files', type=int, default=None, help='number of expert files to read (leave as None unless doing ablations)')
+        parser.add_argument('--max_experts', type=int, default=None, help='number of experts to read per file (leave as None unless doing ablations)')
+
+        parser.add_argument('--force_save', action='store_true', help='this will save images for 50ipc')
+
         args = parser.parse_args()
         args.dsa = True
         args.device = 'cuda'
@@ -67,7 +110,7 @@ def get_cifar10_testset():
 if __name__ == '__main__':
     import sys
     sys.path.append('/home/justincui/dc_benchmark/dc_benchmark')
-    from distilled_results.TrajectoryMatching.trajectory_matching_data_loader import TMDataLoader
+    from distilled_results.TrajectoryMatching.tm_data_loader import TMDataLoader
     from torchvision import datasets, transforms
 
     train_image = TMDataLoader.load_images('/home/justincui/dc_benchmark/dc_benchmark/distilled_results/TrajectoryMatching/CIFAR10/IPC10/images_best_ipc10.pt')
