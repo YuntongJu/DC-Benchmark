af06108ffd61566bf410b3316486af1d782a4b17
justincui03@gmail.com <justincui03@gmail.com>
2022-04-19 11:56:32 -0700

    add data loader for the whole dataset.

---
+++ distilled_results/whole_dataset/data_loader.py
@@ -0,0 +1,30 @@
+
+import torch
+from torchvision import datasets, transforms
+
+class WholeDataLoader:
+
+    @staticmethod
+    def load_data(dataset_name):
+        transform = transforms.Compose([transforms.ToTensor()])
+        if dataset_name == 'cifar10':
+            ds_train = datasets.CIFAR10('data', train=True, download=True, transform=transform)
+        else:
+            ds_train = None
+        images_all = [torch.unsqueeze(ds_train[i][0], dim=0) for i in range(len(ds_train))]
+        labels_all = [ds_train[i][1] for i in range(len(ds_train))]
+
+        images_all = torch.cat(images_all, dim=0)
+        labels_all = torch.tensor(labels_all, dtype=torch.long)
+        return images_all, labels_all
+
+
+
+
+
+
+if __name__ == '__main__':
+    
+    images, labels = WholeDataLoader.load_data('cifar10')
+    print(images.shape)
+    print(labels.shape)
