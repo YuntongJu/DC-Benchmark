b20c1ed51e8d1b4210945ab7bfb93688e95782d9
justincui03@gmail.com <justincui03@gmail.com>
2022-04-20 11:40:05 -0700

    add Kmeans for selecting images.

--- distilled_results/Kmeans/kmeans_data_loader.py
+++ distilled_results/Kmeans/kmeans_data_loader.py
@@ -1,15 +1,38 @@
 import torch
 from torchvision import datasets, transforms
+from sklearn.cluster import KMeans
+import numpy as np
 
 class KMeansDataLoader:
 
     @staticmethod
     def load_data():
         transform = transforms.Compose([transforms.ToTensor()])
+        # transform = transforms.Compose([])
         dataset = datasets.CIFAR10('data', train=True, download=True, transform=transform)
-        print(len(dataset))
+        data_map = {}
+        for i in range(10):
+            data_map[i] = []
+        for data in dataset:
+            data_map[data[1]].append(data[0].resize(3 * 32 * 32).numpy())
+        # Find cluster centers using KMeans.
+        images = []
+        labels = []
+        for key in data_map:
+            X = np.array(data_map[key])
+            print(X.shape)
+            kmeans = KMeans(n_clusters=10, random_state=0).fit(X)
+            for center in kmeans.cluster_centers_:
+                images.append(torch.unsqueeze(torch.from_numpy(center).resize(3, 32, 32), dim=0)) 
+                labels.append(key)
+        images = torch.cat(images)
+        labels = torch.Tensor(labels)
+        return images, labels
 
 
 
 if __name__ == '__main__':
-    train_images, train_labels = KMeansDataLoader.load_data()
+    images, labels = KMeansDataLoader.load_data()
+    print(images.shape)
+    print(labels.shape)
+    
