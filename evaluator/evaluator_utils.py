aedda2113b51159558c52458beb53670d784257e
Justin <justincui03@gmail.com>
2022-05-03 22:14:25 -0700

    support auto-select gpu in EvaluatorUtils.

--- evaluator/evaluator_utils.py
+++ evaluator/evaluator_utils.py
@@ -282,7 +282,14 @@ class EvaluatorUtils:
 
         return dc_aug_param
 
-
+    @staticmethod
+    def pick_gpu_lowest_memory():
+        import gpustat
+        stats = gpustat.GPUStatCollection.new_query()
+        ids = map(lambda gpu: int(gpu.entry['index']), stats)
+        ratios = map(lambda gpu: float(gpu.memory_used)/float(gpu.memory_total), stats)
+        bestGPU = min(zip(ids, ratios), key=lambda x: x[1])[0]
+        return bestGPU
 
 
 
