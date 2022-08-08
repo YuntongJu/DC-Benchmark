9bf933fc735b93471cfcc69c686197934c4a075f
Justin Cui <justincui03@gmail.com>
2022-08-08 14:23:16 -0700

    fix kip data loader.

--- data_loader/kip_data_loader.py
+++ data_loader/kip_data_loader.py
@@ -6,19 +6,18 @@ class KIPDataLoader:
 
     @staticmethod
     def load_data(root_dir, dataset, ipc, data_file):
-        image_path = data_file[0]
-        label_path = data_file[1]
-        image_path = os.path.join(root_dir, "KIP", dataset, 'IPC' + str(ipc), image_path)
-        label_path = os.path.join(root_dir, "KIP", dataset, 'IPC' + str(ipc), label_path)
-        image = torch.from_numpy(np.load(image_path)).permute(0, 3, 1, 2)
-        label = torch.from_numpy(np.load(label_path))
+        data_path = os.path.join(root_dir, "KIP", dataset, 'IPC' + str(ipc), data_file)
+        data = np.load(data_path)
+        image = torch.from_numpy(data['images']).permute(0, 3, 1, 2)
+        label = torch.from_numpy(data['labels'])
         return (image, label)
 
 
 if __name__ == '__main__':
     
-    images, labels = KIPDataLoader.load_data('/nfs/data/justincui/dc_benchmark/distilled_results/', 'CIFAR10', 10, ('images.npy', 'labels.npy'))
+    images, labels = KIPDataLoader.load_data('/nfs/data/justincui/dc_benchmark/distilled_results', 'CIFAR10', 10, 'kip_cifar10_ConvNet_ssize100_zca_nol_noaug_ckpt1000.npz')
     print(images.shape)
     print(labels.shape)
     print(labels.max(), labels.min())
     print(images.max(), images.min())
+    # print((labels + 1/10).long())
